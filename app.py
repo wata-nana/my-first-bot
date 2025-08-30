757080e787b7ff6c9e39d5cd7a09ebbb7ff06faf
wata-nana <tonarinotototro@gmail.com>
2025-08-30 12:20:29 +0900

    4度目の修正

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def callback():
 # WebhookHandlerでリクエストを受け取ってhandle_message関数で受け取ったメッセージをそのまま返信
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="絶対に文中にばかって入れないでね"))
+    TextSendMessage(text="絶対に文中にばかって入れないでね")
     if "ばか" in event.message.text:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="今バカって言いました？"))
     else:
